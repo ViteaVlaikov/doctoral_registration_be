5304309acdc29f17c9bdbf420324ad8f05ae60b8
vitea <vlaikov.viktor2002@gmail.com>
2023-01-03 21:08:21 +0200

    init commit

---
+++ src/test/java/usm/api/doctoral_registration/DoctoralRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package usm.api.doctoral_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctoralRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
