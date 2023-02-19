4ac040b3f4ef265c4157528ca6d35b733788b588
denis <den.negura.education@gmail.com>
2023-02-19 10:34:55 +0200

    edit study mapper

--- src/main/java/usm/api/doctoral_registration/mapper/StudyMapper.java
+++ src/main/java/usm/api/doctoral_registration/mapper/StudyMapper.java
@@ -4,17 +4,18 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 import usm.api.doctoral_registration.dto.student.StudyDto;
+import usm.api.doctoral_registration.mapper.science.SpecialityMapper;
 import usm.api.doctoral_registration.model.student.Study;
 
-@Mapper
 @Component
+@Mapper(uses = {SpecialityMapper.class})
 public interface StudyMapper extends EntityMapper<Study, StudyDto> {
 
     @Override
-    @Mapping(target = "speciality", source = "speciality.id", defaultExpression = "java(0F)")
+//    @Mapping(target = "speciality", source = "speciality.id", defaultExpression = "java(0F)")
     StudyDto toDto(Study entity);
 
     @Override
-    @Mapping(target = "speciality", expression = "java(new Speciality())")
+//    @Mapping(target = "speciality", expression = "java(new Speciality())")
     Study toEntity(StudyDto dto);
 }
