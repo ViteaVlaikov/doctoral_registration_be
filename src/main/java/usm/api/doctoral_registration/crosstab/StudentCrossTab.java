f6780508d6aed2e09a1aa6640aa76d441c57f702
Negura Denis <den.negura.education@gmail.com>
2023-04-26 17:45:54 +0300

    crosstab start working

---
+++ src/main/java/usm/api/doctoral_registration/crosstab/StudentCrossTab.java
@@ -0,0 +1,14 @@
+package usm.api.doctoral_registration.crosstab;
+
+import java.util.List;
+import java.util.Map;
+
+public class StudentCrossTab implements CrossTab {
+
+    List<Item> items;
+
+    public StudentCrossTab(Map<String, String> params) {
+
+    }
+
+}
