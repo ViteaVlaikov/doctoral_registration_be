78364c7633c49eef064b31a246f7deebc7e70981
Victor Vlaicov <victor.vlaicov@iongroup.com>
2023-06-09 12:05:45 +0300

    security enabled

--- src/main/java/usm/api/doctoral_registration/security/JwtTokeFilter.java
+++ src/main/java/usm/api/doctoral_registration/security/JwtTokeFilter.java
@@ -1,32 +1,32 @@
-//package usm.api.doctoral_registration.security;
-//
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class JwtTokeFilter extends OncePerRequestFilter {
-//    private final JwtTokenValidator tokenValidator;
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request,
-//                                    HttpServletResponse response,
-//                                    FilterChain chain)
-//            throws ServletException, IOException {
-//
-//        try {
-//            tokenValidator.validate(request.getHeader(HttpHeaders.AUTHORIZATION));
-//        } catch (RuntimeException e) {
-//            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-//        }
-//        chain.doFilter(request, response);
-//    }
-//}
+package usm.api.doctoral_registration.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokeFilter extends OncePerRequestFilter {
+    private final JwtTokenValidator tokenValidator;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws ServletException, IOException {
+
+        try {
+            tokenValidator.validate(request.getHeader(HttpHeaders.AUTHORIZATION));
+        } catch (RuntimeException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+        chain.doFilter(request, response);
+    }
+}
