bb1d913e30f80cb576f2b80969aa01545c23bc0a
vitea <vlaikov.viktor2002@gmail.com>
2023-06-10 20:56:40 +0300

    spring security enabled

--- src/main/java/usm/api/doctoral_registration/security/JwtTokenValidator.java
+++ src/main/java/usm/api/doctoral_registration/security/JwtTokenValidator.java
@@ -1,26 +1,27 @@
-//package usm.api.doctoral_registration.security;
-//
-//import com.auth0.jwt.JWT;
-//import com.auth0.jwt.interfaces.DecodedJWT;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.Date;
-//
-//import static org.springframework.util.ObjectUtils.isEmpty;
-//
-//@Component
-//public class JwtTokenValidator {
-//
-//    public void validate(String token) {
-//        if (isEmpty(token) || !token.startsWith("Bearer ")) {
-//            throw new RuntimeException();
-//        }
-//        String accessToken = token.substring(7);
-//        DecodedJWT decodedJWT = JWT.decode(accessToken);
-//        Long expireDate = decodedJWT.getExpiresAt().getTime();
-//        Long currentDate = new Date().getTime();
-//        boolean isActive = (expireDate - currentDate) > 0;
-//        if (!isActive)
-//            throw new RuntimeException();
-//    }
-//}
+package usm.api.doctoral_registration.security;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
+
+@Component
+public class JwtTokenValidator {
+
+    public void validate(String token) {
+        if (isEmpty(token) || !token.startsWith("Bearer ")) {
+            throw new RuntimeException();
+        }
+        String accessToken = token.substring(7);
+        DecodedJWT decodedJWT = JWT.decode(accessToken);
+        Long expireDate = decodedJWT.getExpiresAt().getTime();
+        Long currentDate = new Date().getTime();
+        boolean isActive = (expireDate - currentDate) > 0;
+        if (!isActive)
+            throw new RuntimeException();
+    }
+}
